b3d5a984c597e9712d6d655df8ed7308a740090e
Bastien <bastienm95@gmail.com>
2020-11-17 20:50:08 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+
+const listener = app.listen(8080, function () {
+  console.log("Listening on port " + listener.address().port);
+});
